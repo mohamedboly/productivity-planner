7634529a52010619200965a4f36f57d718ec9430
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-10-11 15:20:09 -0400

    (feat) - add register method to add user in firebase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { Auth } from './core/auth';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,11 @@ import { environment } from '../environments/environment';
 export class App {
   url = environment.firebaseConfig.authDomain;
   protected readonly title = signal('productivity-planner');
+  readonly #authentificationService = inject(Auth);
+
+  constructor() {
+    this.#authentificationService.register('layeboly@gmail.com', 'passer123').subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
