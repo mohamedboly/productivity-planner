45541713659fdf572f427155feab804557abc81f
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-10-15 22:39:25 -0400

    feat (home page) - create navbar (#21)

---
+++ src/app/core/components/navbar/navbar.smart.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarLogoComponent } from './navbar-logo/navbar-logo';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NavbarLogoComponent],
+  templateUrl: './navbar.smart.component.html',
+  styleUrl: './navbar.smart.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarSmartComponent {}
