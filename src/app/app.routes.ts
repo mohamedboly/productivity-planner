62b2b97d053e210fa5fa5861ae2dc3b9599769f9
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-10-20 14:14:42 -0400

    Dipendency Inversion Control -port (AuthenticationService) -adapter (AuthenticationFirebseService)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitor/home/home.page.component';
+import { SignupPageComponent } from './visitor/signup/signup.page.component';
 
 export const routes: Routes = [
   {
     title: 'Productivity Planner',
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupPageComponent,
+    title: 'Signup',
   },
 ];
