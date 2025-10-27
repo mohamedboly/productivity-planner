5296c0e1b2cc43d9d44bd7d2e7419fe2037d43ed
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-10-27 16:57:40 -0400

    Feat auth state management (#24)
    * chore (deps) add ngrx/signals
    
    * feat (signal store and authenticationService
    
    * feat (TDD sur le loginComponent

---
+++ src/app/visitor/login/login.page.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  imports: [FormsModule],
+  templateUrl: './login.page.component.html',
+  styleUrl: './login.page.component.scss',
+})
+export class LoginPageComponent {
+  readonly email = signal('');
+  readonly password = signal('');
+}
