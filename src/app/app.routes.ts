b3811d5d9359aa6a53481d7f604ba9d40d0a6500
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-11-27 14:17:32 -0500

    dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './membership/dashboard/dashboard.page.component';
 import { HomePageComponent } from './visitor/home/home.page.component';
+import { LoginPageComponent } from './visitor/login/login.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
 
 export const routes: Routes = [
@@ -12,5 +14,15 @@ export const routes: Routes = [
     path: 'signup',
     component: SignupPageComponent,
     title: 'Signup',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    title: 'Login',
+  },
+  {
+    path: 'app/dashboard',
+    component: DashboardPageComponent,
+    title: 'Dashboard',
   },
 ];
