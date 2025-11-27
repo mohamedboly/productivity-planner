d749586a98e45ca362c4d8edaaf620b74a8f6636
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-11-27 14:43:54 -0500

    generate membership pages

---
+++ src/app/membership/planning/planning.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planning',
+  imports: [],
+  templateUrl: './planning.page.component.html',
+  styleUrl: './planning.page.component.scss'
+})
+export class PlanningPageComponent {
+
+}
