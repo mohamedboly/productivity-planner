7c74fc53ebbdffa9fd583891c69867ccaef777c8
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-11-28 19:45:31 -0500

    feat (membership) add sidebar

---
+++ src/app/membership/core/sidebar/sidebar.smart.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.smart.component.html',
+  styleUrl: './sidebar.smart.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarSmartComponent {}
